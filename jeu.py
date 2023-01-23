5e25ac2de710e304636e5aab50c822decc38da3c
walezkamarrder <116007333+walezkamarrder@users.noreply.github.com>
2023-01-23 09:03:31 -0500

    squelette de base

--- jeu.py
+++ jeu.py
@@ -1,26 +1,64 @@
-class Tableau:
-    def __init__():
-        pass
-
-    # Disposition des tuiles
-    # Disposition des portails
-    # Variété de tableau
+class Board:
+    def __init__(self, rows, columns):
+        self.rows = rows
+        self.columns = columns
 
     def __str__():
         pass
+        
+
+    # Disposition des tuiles
+    # Disposition des portails    
+    # Variété de tableau
+class Tile:
+    # Obstacles
+
+    def __init__():
+        pass
+
+    def __str__():
+        pass
+    
+class Portail(Tile):
+    def __init__():
+        pass
+    def __str__():
+        pass
+
+    
+class Obstacle(Tile):
+    def __init__():
+        pass
+    def __str__():
+        pass
+    
+
+class Chance(Tile):
+    def __init__(): # +/-
+        pass
+     
+    def __str__():
+        pass
+
     # Afficher le tableau
 
 
-
-        
-class Joueur:
+class Player:
     def __init__():
         pass
     def __str__():
         pass
-class Dé:
+
+
+class Startup:
     def __init__():
         pass
-class Jouer:
-    def __init__():
+    def __str__():
         pass
+
+# Demarrage
+
+# Affichage du menu: 
+# Niveaux ex: 8x8 avec 20% d'apparition de portail ( le passer a board) 
+# Instructions 
+# 
