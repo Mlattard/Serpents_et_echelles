359ec66461ba03fbf032bd453e65442f3942439a
walezkamarrder <116007333+walezkamarrder@users.noreply.github.com>
2023-01-23 08:46:04 -0500

    debut

---
+++ jeu.py
@@ -0,0 +1,26 @@
+class Tableau:
+    def __init__():
+        pass
+
+    # Disposition des tuiles
+    # Disposition des portails
+    # Variété de tableau
+
+    def __str__():
+        pass
+    # Afficher le tableau
+
+
+
+        
+class Joueur:
+    def __init__():
+        pass
+    def __str__():
+        pass
+class Dé:
+    def __init__():
+        pass
+class Jouer:
+    def __init__():
+        pass
