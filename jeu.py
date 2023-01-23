de34b4cbb86fbc84a0dbb91010318ad11623d1d4
walezkamarrder <116007333+walezkamarrder@users.noreply.github.com>
2023-01-23 11:03:24 -0500

    idees ajoutees

--- jeu.py
+++ jeu.py
@@ -1,7 +1,26 @@
+# comment implanter le deplacement
+
+
+
 class Board:
     def __init__(self, rows, columns):
         self.rows = rows
         self.columns = columns
+        
+    def board_generator(self):
+        self.board = [[]] # matrice
+
+        # Board de départ
+        # logique avec random de generateur de tuile, if l'un est tombe du le 20% de chance, l'autre ne peut pas
+        # for line in lines:
+        #   for tile in tiles:
+        #       self.tile = Tuile ou Chance ou Obstacle . random
+        #       self.board[row.append(self.tile)]
+
+        # 2 cases pas générée aleatoirement ( depart et arrivée)
+        # si on est funky on pourrait faire bouger la case arrivée
+
+        # quoi faire quand les deux joueurs sont sur la même case
 
     def __str__():
         pass
@@ -9,6 +28,7 @@ class Board:
     # Disposition des tuiles
     # Disposition des portails    
     # Variété de tableau
+    # déplacements: effets selon les cartes
 
 class Tile:
     # Obstacles
@@ -16,7 +36,8 @@ class Tile:
     def __init__():
         pass
 
-    def win():
+    def win_tile():
+        pass
     
 
     def __str__():
@@ -33,33 +54,56 @@ class Portal(Tile):
 class Obstacle(Tile):
     def __init__():
         # visel/representation graphique
+        # def what they do
         pass
     
     
 
 class Chance(Tile):
     def __init__(): # +/-
+        # sa representation
         pass
-     
-    
+    # quan d on tombe sur case chance, on ferait self.bag apped item
+    # méthode qui fait piger random une carte de la classe carte
+    # def what they do
 
-    # Afficher le tableau
 
+class Card:
+    def __init__(self):
+        # ex self.valeur = 1 ou -1
+        ### est ce qu'on fait deux classes différentes (+1, -1) ???
+        pass    
+ 
+class Player(Tile):
+    def __init__(self,):
+        self.bag = [] # Card
 
-class Player:
-    def __init__():
+        # part avec une sac vide et ex tombe sur case chance
+        # on ajoute une carte X a notre main 
+
         # scoreboard
         pass
     def roll_dice():
+        # valeur de deplacement associé selon le lancé de dé.
         pass
     def play_card():
+        # prend la valeur de placement et tu +1 ou -1 selon self.bag if les cartes sont dispo dans self.bag
+        # if +1 présent. input veux tu faire +1
+        #if -1 présent. input veux tu faire -1
+        #if +1 -1 présent. input veux tuf aire +1 ou -1
+
+        #prend valeur deplacement et retourne valeur de dep. modifiée
+
+        # quand t'as ta valeur de dep final
         pass
 
     def move_token():
-        pass
+        # trover moyen de avancer de D/G quand ligne impair et l'autre G/D paire
+
 
     def __str__():
         pass
+    # le player est reprenté 
         
 
 class Ai(Player):
