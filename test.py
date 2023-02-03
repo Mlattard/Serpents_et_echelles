96bbe2709049c63fe8cc34d35ffa1d462bd30faa
walezkamarrder <116007333+walezkamarrder@users.noreply.github.com>
2023-02-02 21:05:58 -0500

    premise modifiee

--- test.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-def menu():
-
-        with open("premise.txt", "r", encoding="utf-8") as premise_game:
-            description = premise_game.read()
-            print(description)
-            subprocess.run("cls", shell=True)
-menu()
